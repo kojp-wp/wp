90ced461478ac86e447db2594244ef5d0db057d0
tkrauo <tkr00st@gmail.com>
2021-02-13 17:26:25 +0900

    2015/11/01 以前の記事は表示させない

--- single.php
+++ single.php
@@ -2,67 +2,78 @@
 $previous_post = get_previous_post();
 $next_post = get_next_post();
 $page_title = get_the_title();
+
+$is_displayed = (get_the_date('Ymd') >= 20151101) ? true : false;
+$is_previous_post_displayed =
+  (get_the_date('Ymd', $previous_post->ID) >= 20151101) ? true : false;
 ?>
-<!DOCTYPE html>
-<html lang="ja">
+<?php if ($is_displayed) : ?>
+  <!DOCTYPE html>
+  <html lang="ja">
 
-<head>
-  <?php
-  get_template_part("head");
-  ?>
-</head>
+  <head>
+    <?php
+    get_template_part("head");
+    ?>
+  </head>
 
-<body>
-  <?php
-  get_header();
-  ?>
-  <main class="main-content-wrapper">
-    <article class="blog-main">
-      <div class="blog-header-nav">
-        <?php if (!empty($previous_post)) : ?>
-          <a href="<?php echo get_permalink($previous_post->ID); ?>" class="blog-header-nav__day-before">
-            <span>前日</span>
+  <body>
+    <?php
+    get_header();
+    ?>
+    <main class="main-content-wrapper">
+      <article class="blog-main">
+        <div class="blog-header-nav">
+          <?php if (!empty($previous_post) && $is_previous_post_displayed) : ?>
+            <a href="<?php echo get_permalink($previous_post->ID); ?>" class="blog-header-nav__day-before">
+              <span>前日</span>
+            </a>
+          <?php endif; ?>
+          <?php if (!empty($next_post)) : ?>
+            <a href="<?php echo get_permalink($next_post->ID); ?>" class="blog-header-nav__day-after">
+              <span>翌日</span>
+            </a>
+          <?php endif; ?>
+        </div>
+        <h2><?php echo post_custom('サブタイトル'); ?></h2>
+        <div class="blog-main__content-height">
+          <div class="blog-main__content">
+            <h3 class="blog-main__content-right"><?php echo $page_title; ?></h3>
+            <div class="blog-main__content-left">
+              <?php echo get_the_content(); ?>
+            </div>
+          </div>
+        </div>
+        <div class="blog-main__card">
+          <img class="blog-main__card" src="/img/blog/blog-one-point.png" alt="blog-one-point">
+        </div>
+      </article>
+
+      <div class="blog-footer-nav">
+        <?php if (!empty($previous_post) && $is_previous_post_displayed) : ?>
+          <a href="<?php echo get_permalink($previous_post->ID); ?>" class="blog-footer-nav__day-before">
+            <span class="blog-footer-nav__day-before-zenjitsu">前日</span>
+            <span class="blog-footer-nav__day-before-title"><?php echo $previous_post->post_title; ?></span>
           </a>
         <?php endif; ?>
         <?php if (!empty($next_post)) : ?>
-          <a href="<?php echo get_permalink($next_post->ID); ?>" class="blog-header-nav__day-after">
-            <span>翌日</span>
+          <a href="<?php echo get_permalink($next_post->ID); ?>" class="blog-footer-nav__day-after">
+            <span class="blog-footer-nav__day-after-title"><?php echo $next_post->post_title; ?></span>
+            <span class="blog-footer-nav__day-after-yokujitsu">翌日</span>
           </a>
         <?php endif; ?>
       </div>
-      <h2><?php echo post_custom('サブタイトル'); ?></h2>
-      <div class="blog-main__content-height">
-        <div class="blog-main__content">
-          <h3 class="blog-main__content-right"><?php echo $page_title; ?></h3>
-          <div class="blog-main__content-left">
-            <?php echo get_the_content(); ?>
-          </div>
-        </div>
-      </div>
-      <div class="blog-main__card">
-        <img class="blog-main__card" src="/img/blog/blog-one-point.png" alt="blog-one-point">
-      </div>
-    </article>
+    </main>
+    <?php
+    get_footer();
+    ?>
+    <script src="/js/blog/script.js"></script>
+  </body>
 
-    <div class="blog-footer-nav">
-      <?php if (!empty($previous_post)) : ?>
-        <a href="<?php echo get_permalink($previous_post->ID); ?>" class="blog-footer-nav__day-before">
-          <span class="blog-footer-nav__day-before-zenjitsu">前日</span>
-          <span class="blog-footer-nav__day-before-title"><?php echo $previous_post->post_title; ?></span>
-        </a>
-      <?php endif; ?>
-      <?php if (!empty($next_post)) : ?>
-        <a href="<?php echo get_permalink($next_post->ID); ?>" class="blog-footer-nav__day-after">
-          <span class="blog-footer-nav__day-after-title"><?php echo $next_post->post_title; ?></span>
-          <span class="blog-footer-nav__day-after-yokujitsu">翌日</span>
-        </a>
-      <?php endif; ?>
-    </div>
-  </main>
+  </html>
+<?php else : ?>
   <?php
-  get_footer();
+  wp_safe_redirect(home_url(), 302);
+  exit;
   ?>
-  <script src="/js/blog/script.js"></script>
-</body>
-
-</html>
+<?php endif; ?>
